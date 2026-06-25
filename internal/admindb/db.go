49381901df295dbcf9342d2da2aa288b11cab6b5
tursom <tursom@foxmail.com>
2026-06-25 11:44:16 +0800

    Fix gateway cross-build without sqlite support

--- internal/admindb/db.go
+++ internal/admindb/db.go
@@ -1,3 +1,5 @@
+//go:build (darwin && (amd64 || arm64)) || (freebsd && (amd64 || arm64)) || (linux && (386 || amd64 || arm || arm64 || loong64 || ppc64le || riscv64 || s390x)) || (openbsd && (amd64 || arm64)) || (windows && (386 || amd64 || arm64))
+
 package admindb
 
 import (
---
+++ internal/admindb/db_unsupported.go
@@ -0,0 +1,17 @@
+//go:build !((darwin && (amd64 || arm64)) || (freebsd && (amd64 || arm64)) || (linux && (386 || amd64 || arm || arm64 || loong64 || ppc64le || riscv64 || s390x)) || (openbsd && (amd64 || arm64)) || (windows && (386 || amd64 || arm64)))
+
+package admindb
+
+import (
+	"database/sql"
+	"fmt"
+	"runtime"
+)
+
+func Open(string) (*sql.DB, error) {
+	return nil, fmt.Errorf("sqlite admin database is not supported on %s/%s", runtime.GOOS, runtime.GOARCH)
+}
+
+func Migrate(*sql.DB) error {
+	return fmt.Errorf("sqlite admin database is not supported on %s/%s", runtime.GOOS, runtime.GOARCH)
+}
